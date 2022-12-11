66601c0360aa79d33822e73147b700e71347c436
imac <dont@mail.com>
2022-12-11 01:42:21 -0600

    Remove useless source files

--- src/glmain.zig
+++
@@ -1,8 +0,0 @@
-const std = @import("std");
-const testing = std.testing;
-
-pub const graphics = @import("graphics.zig");
-pub const gui = @import("gui.zig");
-pub const SparseSet = @import("sparse_set.zig").SparseSet;
-
-test "refalldecls" {}
