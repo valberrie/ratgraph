475d471f11f7dddd297305e531450e7c85a20aa1
imac <dont@mail.com>
2022-12-11 01:37:15 -0600

    Add submodules

---
+++ c_libs/freetype/freetype_init.h
@@ -0,0 +1,18 @@
+#include <ft2build.h>
+#define FT_CONFIG_OPTION_SUBPIXEL_RENDERING
+#include <freetype/freetype.h>
+
+  #undef FTERRORS_H_
+  #define FT_ERRORDEF( e, v, s )  { e, s },
+  #define FT_ERROR_START_LIST     {
+  #define FT_ERROR_END_LIST       { 0, NULL } };
+
+  struct ErrorType{
+    int          err_code;
+    const char*  err_msg;
+  } ;
+
+struct ErrorType ft_errors[] =
+
+  #include <freetype/fterrors.h>
+
